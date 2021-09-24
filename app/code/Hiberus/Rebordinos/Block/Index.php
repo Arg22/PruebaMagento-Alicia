c18eb6b95b3043a1783b5c374e95ca622da0688a
Arg22 <aliciarguzman22@gmail.com>
2021-09-24 11:14:51 +0200

    Panel hecho

--- app/code/Hiberus/Rebordinos/Block/Index.php
+++ app/code/Hiberus/Rebordinos/Block/Index.php
@@ -7,6 +7,8 @@ use Hiberus\Rebordinos\Model\Exam;
 use Hiberus\Rebordinos\Api\Data\ExamInterfaceFactory;
 use Magento\Framework\Data\Collection\AbstractDb;
 use Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
+use Magento\Store\Model\ScopeInterface;
+use Magento\Framework\App\Config\ScopeConfigInterface;
 
 class Index extends \Magento\Framework\View\Element\Template
 {
@@ -16,6 +18,7 @@ class Index extends \Magento\Framework\View\Element\Template
     protected $examRepository;
     protected $examInterfaceFactory;
     protected $examResource;
+    protected ScopeConfigInterface $scopeConfig;
 
     /**
      * @param \Magento\Framework\View\Element\Template\Context $context
@@ -33,6 +36,7 @@ class Index extends \Magento\Framework\View\Element\Template
         ExamRepositoryInterface $examRepository,
         ExamInterfaceFactory $examInterfaceFactory,
         \Hiberus\Rebordinos\Model\ResourceModel\Exam $examResource,
+        ScopeConfigInterface $scopeConfig,
         array $data = []
     ) {
         $this->registry = $registry;
@@ -40,6 +44,7 @@ class Index extends \Magento\Framework\View\Element\Template
         $this->examRepository = $examRepository;
         $this->examInterfaceFactory = $examInterfaceFactory;
         $this->examResource = $examResource;
+        $this->scopeConfig = $scopeConfig;
         parent::__construct($context, $data);
     }
 
@@ -59,5 +64,35 @@ class Index extends \Magento\Framework\View\Element\Template
             $marks[]=$item->getMark();
         }
         return array_sum($marks)/count($exams);
+    }
+
+    public function getMaxMarks(){
+        $total=$this->getExams();
+        $marks=[];
+        $maxMarks=[];
+        $notaMax=0;
+        foreach ($total as $item){
+            $marks[]=$item->getMark();
+        }
+        $max=max($marks);
+        foreach ($marks as $mark){
+            $nota=$mark;
+            if($nota<=$max && count($maxMarks)<3){
+                $notaMax=$nota;
+                $maxMarks[]=$notaMax;
+            }
+        }
+        return $maxMarks;
+    }
+
+    public function getPassNote(){
+        $data= $this->scopeConfig->getValue( 'hiberus_nombre/general/notaMinima', ScopeInterface::SCOPE_STORE);
+        return  $data?:5;
+    }
+
+    public function getSize(){
+        $data= $this->scopeConfig->getValue( 'hiberus_nombre/general/cantidad', ScopeInterface::SCOPE_STORE);
+        $exams = $this->getExams();
+        return  $data?:count($exams);
     }
 }
